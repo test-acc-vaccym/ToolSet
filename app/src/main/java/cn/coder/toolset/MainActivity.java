ff5103d92d47fe40ea35a0a12a3202cb71886f41
LiCheng <tsotumu@qq.com>
2017-08-07 21:37:36 +0800

    修改。

--- app/src/main/java/cn/coder/toolset/MainActivity.java
+++ app/src/main/java/cn/coder/toolset/MainActivity.java
@@ -1,26 +1,16 @@
 package cn.coder.toolset;
 
 import android.support.design.widget.TabLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-   
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
+
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-  
-import android.widget.TextView;
 
-import cn.coder.toolset.view.MyGridView;
-import cn.coder.toolset.view.PlaceholderFragment;
+import cn.coder.toolset.adapter.SectionsPagerAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -43,7 +33,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        initData();
 
+    }
+
+    private void initData() {
+        // TODO: 2017/8/7
+    }
+
+    private void initView() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         // Create the adapter that will return a fragment for each of the three
@@ -51,21 +50,11 @@ public class MainActivity extends AppCompatActivity {
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
         // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager = (ViewPager) findViewById(R.id.page_container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(mViewPager);
-
-      FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-      fab.setOnClickListener(new View.OnClickListener() {
-          @Override
-          public void onClick(View view) {
-              Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                      .setAction("Action", null).show();
-          }
-      });
-      
     }
 
 
@@ -91,41 +80,5 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    
-  
 
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
-        }
-
-        @Override
-        public int getCount() {
-            // Show 3 total pages.
-            return 2;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            switch (position) {
-                case 0:
-                    return "Feature Entry";
-                case 1:
-                    return "App Mgr";
-            }
-            return null;
-        }
-    }
 }
--- app/src/main/java/cn/coder/toolset/view/PlaceholderFragment.java
+++ app/src/main/java/cn/coder/toolset/view/PlaceholderFragment.java
@@ -5,14 +5,21 @@ package cn.coder.toolset.view;
  */
 
 
+import android.app.Application;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 import cn.coder.toolset.R;
+import cn.coder.toolset.model.FeatureSet;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -25,7 +32,8 @@ public class PlaceholderFragment extends Fragment {
     private static final String ARG_SECTION_NUMBER = "section_number";
 
 
-    private MyGridView mMyGridView;
+    private MyGridView mGridView;
+    private FeatureGridAdapter mFeatureAdapter;
 
     /**
      * Returns a new instance of this fragment for the given section
@@ -47,15 +55,72 @@ public class PlaceholderFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = null;
         int pageCount = getArguments().getInt(ARG_SECTION_NUMBER);
-        if (pageCount == 1){
-            rootView = inflater.inflate(R.layout.feature_entry, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, pageCount));
-
-            mMyGridView = (MyGridView) rootView.findViewById(R.id.gv_scan_icons);
-        }else if (pageCount == 2){
-            rootView = inflater.inflate(R.layout.app_mgr, container, false);
+        if (pageCount == 0) {
+            rootView = getFeatureEntryView(inflater, container);
+        } else if (pageCount == 1) {
+            rootView = getAppMgrView(inflater, container);
         }
         return rootView;
+    }
+
+    private View getFeatureEntryView(LayoutInflater inflater, ViewGroup container) {
+        View rootView = inflater.inflate(R.layout.feature_entry, container, false);
+        TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+
+        ArrayList<FeatureSet> featureSets = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++){
+            featureSets.add(new FeatureSet("feature" + i, "des " + i));
+        }
+
+        mGridView = (MyGridView) rootView.findViewById(R.id.feature_grid);
+        mFeatureAdapter = new FeatureGridAdapter(inflater, featureSets);
+        mGridView.setAdapter(mFeatureAdapter);
+        return rootView;
+    }
+
+    private View getAppMgrView(LayoutInflater inflater, ViewGroup container) {
+        View rootView = inflater.inflate(R.layout.app_mgr, container, false);
+        return rootView;
+    }
+
+    private class FeatureGridAdapter extends BaseAdapter {
+        private LayoutInflater mInflater;
+        private ArrayList<FeatureSet> mList = new ArrayList<>();
+        private FeatureGridAdapter(LayoutInflater inflater, ArrayList<FeatureSet> featureSets) {
+            mInflater = inflater;
+            mList.addAll(featureSets);
+        }
+
+        @Override
+        public int getCount() {
+            return mList.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return mList.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(final int position, View contentView, ViewGroup parent) {
+            if (contentView == null) {
+                contentView = mInflater.inflate(R.layout.feature_item, null);
+                ((TextView)contentView.findViewById(R.id.tv_feature_title)).setText(mList.get(position).FeatureTitle);
+                ((TextView)contentView.findViewById(R.id.tv_feature_des)).setText(mList.get(position).FeatureDes);
+                contentView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+//                        guideAction();
+                    }
+                });
+            }
+            return contentView;
+        }
     }
 }
